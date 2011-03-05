a1276dccb24d6e57cf038810311ffefd099d0b50
mortenmarius <mortenmarius@a77e6054-3ab8-4eec-bfea-ca97dac9bd00>
2011-03-05 14:40:29 +0000

    created xsd for level and a script that generates java classes in src folder 'genereated'. added glassfish jaxtb in lib
    git-svn-id: http://kakebox.unfuddle.com/svn/kakebox_mamot/Swashbuckler@6 a77e6054-3ab8-4eec-bfea-ca97dac9bd00

---
+++ generate.bat
@@ -0,0 +1,1 @@
+lib\jaxb-ri-20110115\bin\xjc.bat -d generated/ -p no.mamot.swashbuckler.xml data\level.xsd
---
+++ generate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+lib\jaxb-ri-20110115\bin\xjc.bat -d generated/ -p no.mamot.swashbuckler.xml data\level.xsd
