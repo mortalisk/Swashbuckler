7bf0e9225907c9c86cf6c8ef3d2c1c04cb5d4405
mortenmarius <mortenmarius@a77e6054-3ab8-4eec-bfea-ca97dac9bd00>
2011-03-05 15:02:40 +0000

    fixed some generation errors
    git-svn-id: http://kakebox.unfuddle.com/svn/kakebox_mamot/Swashbuckler@7 a77e6054-3ab8-4eec-bfea-ca97dac9bd00

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lib\jaxb-ri-20110115\bin\xjc.bat -d generated/ -p no.mamot.swashbuckler.xml data\level.xsd
+lib/jaxb-ri-20110115/bin/xjc.sh -d generated/ -p no.mamot.swashbuckler.xml data\level.xsd
