f0f1db43e9df6240cefed66946a3a1fce48d8458
mortenmarius <mortenmarius@a77e6054-3ab8-4eec-bfea-ca97dac9bd00>
2011-03-06 15:43:30 +0000

    camera
    git-svn-id: http://kakebox.unfuddle.com/svn/kakebox_mamot/Swashbuckler@21 a77e6054-3ab8-4eec-bfea-ca97dac9bd00

---
+++ src/no/mamot/swashbuckler/Camera.java
@@ -0,0 +1,58 @@
+package no.mamot.swashbuckler;
+
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Vector2f;
+
+public class Camera {
+
+	private Vector2f topLeftCorner = new Vector2f(0.0f, 0.0f);
+	private int keyUp; 
+	private int keyDown;
+	private int keyLeft;
+	private int keyRight;
+	
+	private float speed = 2.0f;
+	
+	
+	public Camera(int keyUp, int keyDown, int keyLeft, int keyRight){
+		this.keyUp = keyUp;
+		this.keyDown = keyDown;
+		this.keyLeft = keyLeft;
+		this.keyRight = keyRight;
+	}
+	
+	
+	public void handleInput(Input input){
+		if (input.isKeyDown(keyUp)){
+			moveCameraVertical(speed);
+		}
+		if (input.isKeyDown(keyDown)){
+			moveCameraVertical(-speed);
+		}
+		if (input.isKeyDown(keyLeft)){
+			moveCameraHorizontal(speed);
+		}
+		if (input.isKeyDown(keyRight)){
+			moveCameraHorizontal(-speed);
+		}
+		
+		
+		
+	}
+	
+	
+	private void moveCameraVertical (float y){
+		topLeftCorner.y += y;
+		
+	}
+	
+	private void moveCameraHorizontal (float x){
+		topLeftCorner.x += x;
+	}
+
+	public Vector2f getTopLeftCorner() {
+		return topLeftCorner;
+	}
+	
+	
+}
