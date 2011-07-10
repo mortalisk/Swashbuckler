f90cc4a5d7ab99d0ef35889c2325225bb0df93b8
mortenmarius <mortenmarius@a77e6054-3ab8-4eec-bfea-ca97dac9bd00>
2011-07-10 13:35:50 +0000

    git-svn-id: http://kakebox.unfuddle.com/svn/kakebox_mamot/Swashbuckler@97 a77e6054-3ab8-4eec-bfea-ca97dac9bd00

--- src/no/mamot/swashbuckler/GameObstacle.java
+++ src/no/mamot/swashbuckler/GameObstacle.java
@@ -9,6 +9,8 @@ import no.mamot.engine.Drawable;
 import no.mamot.engine.GameObject;
 
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.geom.ShapeRenderer;
@@ -20,8 +22,9 @@ public final class GameObstacle implements GameObject, Drawable {
 
 	private Shape polygon;
 	private Body body;
+	private Image texture;
 
-	public GameObstacle(float[] arg0) {
+	public GameObstacle(float[] arg0, String textureName) {
 		polygon = new Polygon(arg0);
 		ROVector2f[] physPoints = new ROVector2f[arg0.length/2];
 		for (int i = 0; i < physPoints.length; i++) {
@@ -30,10 +33,19 @@ public final class GameObstacle implements GameObject, Drawable {
 		net.phys2d.raw.shapes.Polygon physPolygon = new net.phys2d.raw.shapes.Polygon(physPoints);
 		body = new StaticBody("WorldObstacle",physPolygon);
 		body.setFriction(0.1f);
+		
+		
+		try {
+			texture = new Image("data/textures/"+textureName);
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 
 	public void draw(Graphics g) {
 		ShapeRenderer.draw(this.getShape());
+		ShapeRenderer.texture(this.getShape(), texture);
 	}
 
 	private Shape getShape() {
